9aa61491e00e02a1c035c7b6854df4173174138a
snanousaid <s.snanou@nextronic.io>
2026-04-29 11:04:09 +0100

    fix

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 def generate_frames():
     camera = cv2.VideoCapture(1)
-    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 9999)
-    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 9999)
+    camera.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+    camera.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
     try:
         while True:
             success, frame = camera.read()
